a90501fdf299a7fac122034272a463ed2d8abb02
Corentin Cailleaud <corentin.cailleaud@epitech.eu>
2022-01-28 16:52:55 +0100

    New server with macro

--- Backend/server/index.js
+++ Backend/server/index.js
@@ -15,15 +15,16 @@ const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS
 
 const INTERVAL_READ_SECONDS = 1 // Read each second
 
-const INFURA_URL = `https://${IS_DEV ? "rinkeby" : "mainnet"}.infura.io/v3/be63d41aaacb46639be813ab0e549488`
+const INFURA_URL = `wss://${IS_DEV ? "rinkeby" : "mainnet"}.infura.io/ws/v3/be63d41aaacb46639be813ab0e549488`
 
 const startDate = (new Date()).getTime() / 1000 // Save current timestamp to avoid interpreting old requests
-const web3 = new Web3(new Web3.providers.HttpProvider(INFURA_URL))
+const web3 = new Web3(INFURA_URL)
 
 const fs = require('fs')
 const BITE_SIZE = 256;
 let file, readbytes = 0;
 
+const listPendingTransaction = {}
 let isWaiting = false
 
 async function openFile() {
@@ -55,7 +56,6 @@ function readLine(err, bytecount, buff) {
         const value = line.split(LOG_PREFIX)[1]
         let [time, address, cmd, extradata] = value.split(',')
         if (time && time > startDate) {
-            console.log(cmd + " " + address)
             cmd = cmd.replace(/(\r\n|\n|\r)/gm,"")
             address = address.replace(/(\r\n|\n|\r)/gm,"")
             processCmd(cmd, address, extradata)                
@@ -76,21 +76,26 @@ const getAbiFromAddress = async (contractAddress, apiKey) => {
     })
 }
 
-function sendTransaction(method) {
-    web3.eth.getTransactionCount(PUBLIC_ADDRESS).then(txCount => {
-        const txData = {
-            nonce: web3.utils.toHex(txCount),
-            gasLimit: web3.utils.toHex(300000),
-            gasPrice: web3.utils.toHex(15e9), // 15 Gwei
-            to: CONTRACT_ADDRESS,
-            from: PUBLIC_ADDRESS,
-            chainId: IS_DEV ? 4 : 1,
-            data: method.encodeABI()
-        }
-        sendSigned(txData, function (err, result) {
-            if (err) return console.log('error', err)
-            console.log('Successfully sent (transaction: ', result, ')')
-        })
+async function sendTransaction(method) {
+    return new Promise((resolve, reject) => {
+        web3.eth.getTransactionCount(PUBLIC_ADDRESS).then(txCount => {
+            const txData = {
+                nonce: web3.utils.toHex(txCount),
+                gasLimit: web3.utils.toHex(300000),
+                gasPrice: web3.utils.toHex(15e9), // 15 Gwei
+                to: CONTRACT_ADDRESS,
+                from: PUBLIC_ADDRESS,
+                chainId: IS_DEV ? 4 : 1,
+                data: method.encodeABI()
+            }
+            sendSigned(txData, function (err, result) {
+                if (err) {
+                    reject(err)
+                }
+                console.log('Successfully sent (transaction: ', result, ')')
+                resolve(result)
+            })
+        })    
     })
 }
 
@@ -116,22 +121,73 @@ const COMMANDS = {
 async function processCmd(cmd, address) {
     if (!NFTContract || !cmd || !address) return
     address = address.toLowerCase()
+    console.log(cmd + " " + address)
     const func = COMMANDS[cmd]
     if (!func) {
         console.error(`Error: invalid command ${cmd}.`)
         return
     }
-    func(address)
+    const transactionId = await func(address)
+    listPendingTransaction[transactionId] = { method: cmd, address }
+    console.log(transactionId, listPendingTransaction[transactionId])
     console.log("Sent transaction levelUp for address '" + address + "'")
+}
+
+async function readLogs() {
+    file = await openFile()
+    readFile()
+}
+
+const robot = require("robotjs");
+async function sendDataBackToCore(data) {
+    robot.moveMouse(30, 60);
+    robot.mouseClick()
+    await (new Promise((r,err) => setTimeout(r, 1000)))
+    robot.moveMouse(500, 500);
+    robot.mouseClick()
+    await (new Promise((r,err) => setTimeout(r, 1000)))
+    robot.keyTap("enter");
+    robot.typeString(data);
+    robot.keyTap("enter");
+    robot.moveMouse(30, 60);
+    robot.mouseClick()
+}
+
+async function receivedTransactionConfirmation(event) {
+    const data = listPendingTransaction[event.transactionHash]
+    console.log(data)
+    if (!data) return
+    console.log("Successfully minted")
+    const tokenId = event.returnValues.tokenId
+    console.log('https://testnets.opensea.io/assets/0x01c2edc73bfc68e8b01784d925600c2072d8312e/' + tokenId)
+    // Macro send data back to Core
+    sendDataBackToCore("Success_" + data.method + "_" + data.address.toLowerCase())
 }
 
 const main = async () => {
     const contract_abi = await getAbiFromAddress(CONTRACT_ADDRESS, ETHERSCAN_API_KEY)
     NFTContract = new web3.eth.Contract(contract_abi, CONTRACT_ADDRESS);
 
-    file = await openFile()
-    readFile()
+    
+    let options = {
+        filter: {
+            value: [],
+        },
+        fromBlock: 0
+    };
+
+    NFTContract.events.Transfer(options)
+        .on('data', receivedTransactionConfirmation)
+        .on('changed', changed => console.log("changed", changed))
+        .on('error', err => console.error(err))
+        .on('connected', str => console.log("connected", str))
+
+    const transactionId = await sendTransaction(NFTContract.methods.mint('0x1317fDa26cdac538C3C9BC51AD73a408674674B7'))
+    listPendingTransaction[transactionId] = { method: 'mint', address: '0x1317fDa26cdac538C3C9BC51AD73a408674674B7' }
+
+    readLogs()
 }
+
 
 main()
 
